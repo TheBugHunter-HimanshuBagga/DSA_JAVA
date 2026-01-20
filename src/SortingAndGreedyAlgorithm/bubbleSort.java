5a6fb22dc95fc13ffe15a8142c72eeb69ea7d31a
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-01-20 23:59:41 +0530

    Sorting

--- src/SortingAndGreedyAlgorithm/bubbleSort.java
+++ src/SortingAndGreedyAlgorithm/bubbleSort.java
@@ -16,7 +16,7 @@ public class bubbleSort {
         int n = a.length;
         for(int i = 0 ; i < n-1 ; i++){ // number of iterations [5,3,4,1,2] -> [3,4,1,2,5] i = 0
             for(int j = 0 ; j < n - i - 1 ; j++){//
-                if(a[j] < a[j+1]){
+                if(a[j] > a[j+1]){
                     int temp = a[j+1];
                     a[j+1] = a[j];
                     a[j] = temp;
