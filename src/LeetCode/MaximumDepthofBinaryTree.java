36b77b4ce837364b2b6f623d76e20b3905113975
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-01-23 23:12:54 +0530

    104

---
+++ src/LeetCode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+import javax.swing.tree.TreeNode;
+
+public class MaximumDepthofBinaryTree {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
