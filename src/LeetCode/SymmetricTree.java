1f455d162b0552f6cdfda23038721638a8f30e94
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-01-24 11:46:17 +0530

    101

---
+++ src/LeetCode/SymmetricTree.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import javax.swing.tree.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return helper(root.left , root.right);
+    }
+    static boolean helper(TreeNode a , TreeNode b){
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+
+        if(a.val != b.val) return false;
+        return helper(a.left , b.right) && helper(a.right , b.left);
+    }
+}
