88327bbcce0dafc98293b140c9ce15fc32da619c
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-02-08 23:57:31 +0530

    NodesAtKDistance

--- src/BinaryTree/BinaryTree2.java
+++ src/BinaryTree/BinaryTree2.java
@@ -1,16 +1,41 @@
 package BinaryTree;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BinaryTree2 {
-    public static void main(String[] args){
-        System.out.println(areCousins(root , 4 , 5));
-        ArrayList<Integer> arr = new ArrayList<>();
-        nodesAtLevelK(root , 2 , ans);
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
     }
 
     static Node xPar , yPar;
     static int xLevel, yLevel;
+
+    public static void main(String[] args){
+
+        // BUILD TREE
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.right = new Node(5);
+
+        System.out.println(areCousins(root , 4 , 5));
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        nodesAtLevelK(root , 2 , ans);
+        System.out.println(ans);
+
+        System.out.println(nodesAtLevelK(root , 2));
+    }
 
     static void areCousinsHelper(Node node , Node par , int x , int y , int level){
         if(node == null) return;
@@ -23,15 +48,40 @@ public class BinaryTree2 {
             yPar = par;
             yLevel = level;
         }
+
         areCousinsHelper(node.left , node , x , y , level+1);
-        areCousinsHelper(node.right , node , x ,y , level+1);
+        areCousinsHelper(node.right , node , x , y , level+1);
+    }
+    static class Pair{
+        Node node;
+        int level;
+
+        public Pair(Node node , int level){
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    static void nodesAtLevelK(Node root , int level){
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayDeque<Pair> stack = new ArrayDeque<>();
+
+        Pair first = new Pair(root , level);
+        stack.push(first);
+
+        while(!stack.isEmpty()){
+            Pair curr = stack.poll();
+            if(curr.level == 0){
+                ans.add(curr.node.data);
+            }
+            stack.push(new Pair(curr.node.right , curr.level-1));
+            stack.push(new Pair(curr.node.left , curr.level-1));
+        }
     }
 
     static boolean areCousins(Node root , int x , int y){
         areCousinsHelper(root , null , x , y , 0);
-        boolean onSameLevel = xLevel == yLevel;
-        boolean diffrentParents = xPar != yPar;
-        return onSameLevel && diffrentParents; // if true then cousins
+        return xLevel == yLevel && xPar != yPar;
     }
 
     static void nodesAtLevelK(Node root , int level , ArrayList<Integer> ans){
@@ -43,16 +93,4 @@ public class BinaryTree2 {
         nodesAtLevelK(root.left , level-1 , ans);
         nodesAtLevelK(root.right , level-1 , ans);
     }
-
-    class Node{
-        int data;
-        Node left;
-        Node right;
-
-        public Node(int data){
-            this.data = data;
-        }
-    }
 }
-
-
