e7137a13ce85f880826b49e7a30d3d1639da0d47
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-03-02 14:26:11 +0530

    GenericClasses

---
+++ src/JAVA/WrapperClassesAndGenerics/GenericClasses.java
@@ -0,0 +1,32 @@
+package JAVA.WrapperClassesAndGenerics;
+/*
+Using generics it is possisble to create classes the work with diffrent data types
+ */
+
+public class GenericClasses {
+    public static void main(String[] args) {
+        Dog<String, String> d1 = new Dog<>("asdf123", "Leo");
+        Dog<String, Integer> d2 = new Dog<>("oeiur12", 123);
+
+        Dog<Integer, String> d3 = new Dog<>(12, "Leo");
+
+        System.out.println(d3.getId());
+
+//        ArrayList<Integer> a = new ArrayList<>();
+    }
+}
+
+class Dog<E, AB> {
+    E id;
+    AB name;
+
+    public Dog(E id, AB name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    E getId() {
+        return id;
+    }
+}
+
