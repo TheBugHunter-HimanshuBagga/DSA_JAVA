20ae42fefeeecb01d5a06e2569a757ead5f56fa7
Himanshu Bagga <baggahimanshu311@gmail.com>
2026-06-30 14:22:53 +0530

    Time: 1 ms (96.84%), Space: 43 MB (46.76%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,15 +1,45 @@
+// class Solution {
+//     public int countGoodSubstrings(String s) {
+//         // brute force
+//         int count = 0;
+//         for(int i =0 ; i <= s.length()-3;i++){
+//             char first = s.charAt(i);
+//             char second = s.charAt(i+1);
+//             char third = s.charAt(i+2);
+//             if(first != second && first != third && second != third){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
+
     public int countGoodSubstrings(String s) {
-        // brute force
+
+        int left = 0;
         int count = 0;
-        for(int i =0 ; i <= s.length()-3;i++){
-            char first = s.charAt(i);
-            char second = s.charAt(i+1);
-            char third = s.charAt(i+2);
-            if(first != second && first != third && second != third){
-                count++;
+
+        for(int right = 0; right < s.length(); right++) {
+
+            if(right - left + 1 == 3) {
+
+                char first = s.charAt(left);
+                char second = s.charAt(left + 1);
+                char third = s.charAt(left + 2);
+
+                if(first != second &&
+                   first != third &&
+                   second != third) {
+
+                    count++;
+                }
+
+                left++;
             }
         }
+
         return count;
     }
 }
