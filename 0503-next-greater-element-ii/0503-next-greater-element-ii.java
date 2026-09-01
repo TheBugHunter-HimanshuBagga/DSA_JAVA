c6f20bfca8b62980064d86ae81e7e3f8c55f7727
Himanshu Bagga <baggahimanshu311@gmail.com>
2026-09-01 13:34:37 +0530

    Time: 10 ms (79.47%), Space: 48.4 MB (69.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,31 +1,102 @@
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n = nums.length;
+//         int[] result = new int[n];
+//         for(int i=0;i<nums.length;i++){
+//             int num = nums[i];  
+//             result[i] = -1;
+//             boolean found = false;
+//             for(int j = i+1; j < nums.length; j++){
+//                 if(num < nums[j]){
+//                     result[i] = nums[j];
+//                     found = true;
+//                     break;
+//                 }// do nothing
+//             }
+//             if(!found){
+//                 if(result[i] == -1){
+//                     for(int j = 0; j < i; j++){
+//                         if(num < nums[j]){
+//                             result[i] = nums[j];
+//                             found = true;
+//                             break;
+//                         }
+//                     }
+//                 }
+//             }
+            
+//         }
+//         return result;
+//     }
+// }
+// // num = 1
+
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         // using Monotonic stack'
+//         Stack<Integer> stack = new Stack<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int[] result = new int[nums.length];
+//         for(int i = 0 ; i < nums.length; i++){
+//             int val = nums[i];
+//             if(stack.isEmpty()){
+//                 stack.push(val);
+//             }
+//             else{ // stack is not empty
+//                 // check weather 
+//                 if(val > stack.peek()){
+//                     while(!stack.isEmpty() && val > stack.peek()){
+//                         int popped = stack.pop();
+//                         map.put(popped, val);
+//                     }
+//                     stack.push(val);
+//                 }
+//                 else{
+//                     // val is less than stack.peek then
+//                     stack.push(val);
+//                 }
+//             }
+//         }
+//         for(int k = 0 ; k < nums.length ; k++){
+//             result[k] = map.getOrDefault(nums[k] ,-1);
+//         }
+//         return result;
+//     }
+// }
+// num = 1
+
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+
+        Stack<Integer> stack = new Stack<>();
         int n = nums.length;
-        int[] result = new int[n];
-        for(int i=0;i<nums.length;i++){
-            int num = nums[i];  
-            result[i] = -1;
-            boolean found = false;
-            for(int j = i+1; j < nums.length; j++){
-                if(num < nums[j]){
-                    result[i] = nums[j];
-                    found = true;
-                    break;
-                }// do nothing
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+
+        
+
+        for(int i = 0; i < 2 *nums.length; i++){
+              
+              int index = i % n;
+            int val = nums[index];
+
+            // Find next greater element
+            while(!stack.isEmpty() && val > nums[stack.peek()]){
+
+                int popped = stack.pop();
+
+                result[popped] = val;
             }
-            if(!found){
-                if(result[i] == -1){
-                    for(int j = 0; j < i; j++){
-                        if(num < nums[j]){
-                            result[i] = nums[j];
-                            break;
-                        }
-                    }
-                }
+
+            // Only push during the first pass
+            if(i < n){
+                stack.push(index);
             }
-            
+
+
         }
+
         return result;
     }
 }
-// num = 1
