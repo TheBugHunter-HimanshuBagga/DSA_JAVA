3958133baeb25925fbecfefeb5a8ed4e915fcd78
Himanshu Bagga <baggahimanshu311@gmail.com>
2026-09-09 02:10:14 +0530

    Time: 2 ms (23.78%), Space: 43.9 MB (30.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,37 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int[] x = new int[m+n]; // [0,0,0,0,0,0]
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                x[k] = nums1[i];
-                i++;
+        int[] result = new int[nums1.length];
+        int count = 0;
+        for(int i = 0 ; i < m ; i++){
+            if(nums1[i] != 0){
+                result[count] = nums1[i];
+                count++;
             }
-            else{
-                x[k] = nums2[j];
-                j++;
-            }
-            k++;
-        }
-        while(i < m){
-            x[k] = nums1[i];
-            i++;
-            k++;
-        }
-        while(j < n){
-            x[k] = nums2[j];
-            j++;
-            k++;
         }
 
-        for(int z = 0 ; z < x.length ; z++){
-            nums1[z] = x[z];
+        int newCount = count;
+
+        for(int j = 0 ; j < n ; j++){
+            result[newCount] = nums2[j];
+            newCount++;
+        }
+
+        int p = 0;
+        for(int k = 0; k < result.length ; k++){
+            nums1[p] = result[k];
+            p++;
+        }
+
+        // Arrays.sort(nums1);
+
+        for(int x = 0 ; x < nums1.length ; x++){
+            for(int y = x + 1; y < nums1.length ; y++){
+                if(nums1[x] > nums1[y]){
+                    int temp = nums1[x];
+                    nums1[x] = nums1[y];
+                    nums1[y] = temp;
+                }
+            }
         }
     }
 }
